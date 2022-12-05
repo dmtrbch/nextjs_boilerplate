7a501c1a00648002d9eb5542a2298292edb3c182
bochvarovskidimitar@gmail.com <bochvarovskidimitar@gmail.com>
2022-12-05 19:06:47 +0100

    build: implement storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,45 @@
+import * as NextImage from 'next/image';
+import '../styles/globals.css';
+
+const BREAKPOINTS_INT = {
+  xs: 375,
+  sm: 800,
+  md: 1366,
+  lg: 1920,
+  xl: 2560,
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: { viewports: customViewports },
+};
